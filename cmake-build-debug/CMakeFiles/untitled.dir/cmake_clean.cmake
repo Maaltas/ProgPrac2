7ab7beb551329d7b14938c69140ba0af70600f47
aRetr0 <gseguraa@edU.tecnocampus.cat>
2023-10-24 16:59:17 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/Source/main.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/main.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Source/src/Comodi.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/src/Comodi.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Source/src/Contenidor.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/src/Contenidor.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Source/src/Element.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/src/Element.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Source/src/Lletra.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/src/Lletra.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Source/src/Seleccio.cpp.obj"
+  "CMakeFiles/untitled.dir/Source/src/Seleccio.cpp.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
